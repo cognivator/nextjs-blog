46ddec32f07d0840c3ea41a1e0bee6fe00c99016
cognivator (slhenty) <slhenty@cognivator.com>
2023-10-02 10:51:52 -0400

    feat (home): update blog description

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ export default function Home({allPostsData}) {
       <title>{siteTitle}</title>
     </Head>
     <section className={utilStyles.headingMd}>
-      <p>Cognitive Innovator</p>
+      <p>Cognitive Innovator - the intersection of Cognitive Science and Innovation.</p>
     </section>
     <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
       <h2 className={utilStyles.headingLg}>Blog</h2>
